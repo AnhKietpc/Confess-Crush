6be3a7a7a916a8c0b24dbfa6458f92df457f3b35
AnhKietpc <99584103+AnhKietpc@users.noreply.github.com>
2022-02-13 15:30:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Cho tao xin lỗi nhaaaa",
+    introTitle: 'Khánh Minh ',
+    introDesc: `Tao cực kì xin lỗi
+    về dụ lần trc chat trên tổ `,
+    btnIntro: 'Tao đâu phải đồ chơi đâu mà xin lỗi!!!',
+    title: 'Nhưng cho tao xin lỗi màaaaaaa',
+    desc: 'Cho tao xin lỗi một cách thật lòng mà... ',
+    btnYes: 'Tha lỗi',
+    btnNo: 'Dell',
+    question: 'Thế tha lỗi cho tao nhé :3',
+    btnReply: 'Được',
+    reply: 'OK Tha lỗi ',
+    mess: 'Tao bt mày sẽ tha lỗi cho tao mà =))',
+    messDesc: 'Tao hứa sẽ ko có lần sao nx ',
+    btnAccept: 'Tạm chấp nhận ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
