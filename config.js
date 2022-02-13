2db68e7d7212d1718ee372cd6541d406b57ca0fd
AnhKietpc <99584103+AnhKietpc@users.noreply.github.com>
2022-02-13 15:04:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Cho Tao xin lỗi nha ",
+    introTitle: 'K.minh cho tao xin lỗi đc ko ಥ_ಥ',
+    introDesc: `do lúc trước tao lỡ trê mày nên bây h tao thành thật muốn xin lỗi mày`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Mày cho tao xin lỗi đc kooooooooooo',
+    desc: 'Tao cực kì xin lỗi mày',
+    btnYes: 'OK chấp nhận ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Mày cho tao xin lỗi nha ',
+    btnReply: 'Được ',
+    reply: 'Cho tao xin lỗi nha ',
+    mess: 'Mình biết là bạn sẽ tha lỗi cho mình mà ',
+    messDesc: 'Vậy làm lành nhaaaaa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
