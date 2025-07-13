b86d51b91bcd30732c7493eef5d0a1e3709d8f0b
AnhKietpc <99584103+AnhKietpc@users.noreply.github.com>
2025-07-13 12:02:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    titleWeb: "Gửi cho Bi aka Châu Anh",
+    introTitle: 'Tiện tay đang làm cho Lam thì tôi Lam cho bà luôn',
+    introDesc: `Tôi biết là bà hóng cái đó dữ lắm , lần nào nghe call với tôi bà cũng nhắc hết trơn á =)) nhma phải có dịp mới gửi được`,
+    btnIntro: 'Tiếp Tục Ha',
+    title: 'Phải Chăng bà đang hóng lắm phải không',
+    desc: 'Nhưng mà cố đợi đi nha pà',
+    btnYes: 'Ocie Biết rùi',
+    btnNo: 'Không Quan Tâm',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
